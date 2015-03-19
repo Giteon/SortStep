a1892d495aee1ffc8ef5e7df2aa545b3758a4429
Giteon <fox.gideon@gmail.com>
2015-03-19 11:40:44 -0700

    Update Sorts.java

--- Sorts.java
+++ Sorts.java
@@ -4,22 +4,87 @@ public class Sorts
 {
   public void bubbleSort(int[] list)
   {
-    //your code here
+    for(int outer = 0; outer < list.length-1;outer++)
+    {
+      for (int inner = 0; inner < list.length-1;inner++)
+      {
+        if (list[inner] > list[inner + 1])
+        {
+          int temp = list[inner];
+
+          list[inner] = list[inner + 1];
+
+          list[inner + 1] = temp;
+        }
+      }
+    }
   }
 
   public void selectionSort(int[] list)
   {
-    //your code here
+    int flag, temp;
+
+  for (int outer = 0; outer < list.length - 1; outer++)
+
+  {
+
+    flag = outer;  
+
+    for (int inner = outer + 1; inner < list.length;inner++)
+
+    {
+
+      if (list[inner] < list[flag])
+
+      {
+
+        flag = inner;
+
+      }
+
+    }
+
+    //swap list[outer] & list[flag]
+
+    temp = list[outer];
+
+    list[outer] = list[flag];
+
+    list[flag] = temp;
+
+  }
   }
 
   public void insertionSort(int[] list)
   {
-    //your code here
+    for (int outer = 1; outer < list.length; outer++)
+
+  {
+
+    int position = outer;
+
+    int key = list[position];
+
+    // Shift larger values to the right
+
+    while (position > 0 && list[position - 1] > key)
+
+    {
+
+      list[position] = list[position - 1];
+
+      position--;
+
+    }
+
+    list[position] = key;
+
+  }
   }
 
   private void merge(int[] a, int first, int mid, int last)
   {
-    //your code here
+
   }
 
   public void mergeSort(int[] a, int first, int last)
